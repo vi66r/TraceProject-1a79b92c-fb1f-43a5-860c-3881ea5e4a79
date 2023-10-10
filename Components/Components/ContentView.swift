27d94c3853021cf2d48f618e12dabec055694bd6
vi66r <rafi.rizwan.616@gmail.com>
2023-10-10 16:19:40 -0400

    Update Components/Components/ContentView.swift

--- Components/Components/ContentView.swift
+++ Components/Components/ContentView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     Text("Hello World, this is a test of things updating now that we have a product which is becoming easier and better to use continuously!")
+    Text("We are going to change the code again here with some simple text changes and see if it does what we want it to do!")
     // write your Swift UI code here
   }
 }
